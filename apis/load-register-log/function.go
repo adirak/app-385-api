05f55068da1519073842550af187f9843f031260
adirak <adirak@techberry.co.th>
2020-10-02 23:39:50 +0700

    add apis file

---
+++ apis/load-register-log/function.go
@@ -0,0 +1,38 @@
+// Package apis contains an APIs for frontend
+package apis
+
+import (
+	"net/http"
+
+	"github.com/adirak/app-385-core/apis/regis"
+	"github.com/adirak/app-385-core/util"
+)
+
+// Service is function to handle http request and pass data to other function
+func Service(w http.ResponseWriter, r *http.Request) {
+
+	// Make ReqtData
+	reqt, err := util.GetReqtData(r)
+
+	if err == nil {
+
+		// Call function
+		// You can chage the function that you want to call here
+		// Call SaveRegisterLog function
+		// ============================
+
+		resp := regis.LoadRegisterLog(reqt)
+
+		// ============================
+
+		// Response JSON data to frontend
+		util.SetJSONResponse(resp, w)
+
+	} else {
+
+		// Response JSON data to frontend
+		util.SetBadRequestJSONResponse(err, w)
+
+	}
+
+}
---
+++ apis/load-register-log/go.mod
@@ -0,0 +1,5 @@
+module github.com/adirak/app-385-api
+
+go 1.13
+
+require github.com/adirak/app-385-core v0.0.0-4
